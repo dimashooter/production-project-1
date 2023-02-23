058c2e0eeaa7e1389e14ca9f8724092f84c26994
dmitriy.kranin <dimashooter.dk@gmail.com>
2023-02-23 11:52:39 +0100

    lint fixes

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,7 +10,11 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
+    config.resolve.modules = [
+        path.resolve(__dirname, '../../src'),
+        'node_modules',
+    ];
+    config.resolve.modules = [paths.src, 'node_modules'];
     config.resolve.extensions.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
@@ -30,6 +34,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.plugins.push(new DefinePlugin({
         __IS_DEV__: true,
     }));
+    config.resolve.modules = [
+        path.resolve(__dirname, '../../src'),
+        'node_modules',
+    ];
 
     return config;
 };
